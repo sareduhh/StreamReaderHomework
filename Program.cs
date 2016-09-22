945a0ffe25fea9ad818b6ec92e3afd4f78b77f33
sareduhh <sarahe.kaminski@gmail.com>
2016-09-22 14:15:14 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StreamReaderHomework
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+           
+        //Create an instance of StreamReader
+        StreamReader reader = new StreamReader("..\\..\\..\\StreamReaderHomework.txt");
+            string line = reader.ReadToEnd();
+
+            //print the first line only
+             Console.WriteLine(line);
+            
+            string vowels = line.Replace("a", "A").Replace("e", "E").Replace("i", "I").Replace("o", "O").Replace("u", "U");
+
+            Console.WriteLine(vowels);
+
+     
+            reader.Close();
+
+       
+
+
+        }
+
+
+    }
+    
+}
